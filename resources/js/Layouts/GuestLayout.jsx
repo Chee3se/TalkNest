c355a6675f362c25b1a94d8f08a8f8a45cb0f873
EdmundsEglitis <edmunds3011@gmail.com>
2024-10-15 10:40:52 +0300

    register design change

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,17 +1,19 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                {children}
+        <div
+        
+            className="flex min-h-screen flex-col items-center justify-center p-6 sm:pt-0 bg-cover bg-center"
+            style={{ backgroundImage: "url('/orange-background.png')" }} 
+        >
+            {/* Main Content Box */}
+            <div className="relative w-full overflow-hidden bg-gradient-to-b from-white via-orange-300 to-red-400 px-10 py-8 shadow-xl sm:max-w-md sm:rounded-lg">
+  
+                {/* Main Content */}
+                <div className="relative z-10">
+                    {children}
+                </div>
             </div>
         </div>
     );
