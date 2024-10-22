b0e4e648ba8c54b541ac0c83032c5efe8b525c6b
Ralfs <ralfscd@gmail.com>
2024-10-22 08:46:55 +0300

    Rabba

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,9 +4,20 @@ import Layout from "@/Layouts/Layout.jsx";
 export default function Home({ auth, laravelVersion, phpVersion }) {
     return (
         <Layout header={"Home"}>
+<<<<<<< HEAD
             <div className="flex w-full justify-center ">
                 <h1 className="pt-48 text-8xl font-bold">Yello</h1>
+=======
+            <div className="flex w-full justify-center">
+                <h1 className="pt-48 text-8xl font-bold">Welcome to TalkNest!</h1>
+>>>>>>> 1d7f624 (Rabba)
             </div>
+
+
+            <div className="flex w-full justify-center">
+            <h2 className="pt-64 text-8xl font-bold place-content-center">rah</h2>
+            </div>
+            
         </Layout>
     );
 }
