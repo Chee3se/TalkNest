7345901d4ce1b09bf7d53ca9d274e0f3c342977b
Ralfs <ralfscd@gmail.com>
2024-10-22 10:58:05 +0300

    Home page edit

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,20 +4,16 @@ import Layout from "@/Layouts/Layout.jsx";
 export default function Home({ auth, laravelVersion, phpVersion }) {
     return (
         <Layout header={"Home"}>
-<<<<<<< HEAD
-            <div className="flex w-full justify-center ">
-                <h1 className="pt-48 text-8xl font-bold">Yello</h1>
-=======
-            <div className="flex w-full justify-center">
-                <h1 className="pt-48 text-8xl font-bold">Welcome to TalkNest!</h1>
->>>>>>> 1d7f624 (Rabba)
-            </div>
+            <div style={{ fontFamily: "'Exo 2', sans-serif" }} className="flex w-full justify-center">
+                <h1 className="pt-96 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500 text-3xl font-black">Welcome to TalkNest!</h1>
 
-
-            <div className="flex w-full justify-center">
-            <h2 className="pt-64 text-8xl font-bold place-content-center">rah</h2>
             </div>
-            
+            <div style={{ fontFamily: "'Exo 2', sans-serif" }} className="flex w-full justify-center text-transparent bg-clip-text bg-gradient-to-r from-gray-900 to-gray-500 text-3xl font-black">
+            <h2 className="pt-40 text-4xl font-bold place-content-center">Write new posts, get feedback from people, explore!</h2>
+            </div>
+            <div style={{ fontFamily: "'Exo 2', sans-serif" }} className="flex w-full justify-center text-transparent bg-clip-text bg-gradient-to-r from-gray-900 to-gray-500 text-3xl font-black">
+            <h2 className="pt-96 text-sm font-bold place-content-center">(Click on the 'Posts' tab to begin!)</h2>
+            </div>
         </Layout>
     );
 }
